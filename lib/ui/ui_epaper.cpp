90049c983b0414ee6089b6eada88924f9c20e824
Jeff Cooper <jeff@thejeffcooper.com>
2019-09-02 17:37:53 -0400

    broken epaper ui init (stack overflow?)

--- lib/ui/ui_epaper.cpp
+++ lib/ui/ui_epaper.cpp
@@ -5,29 +5,59 @@
 #include <Fonts/FreeMonoBold9pt7b.h>
 
 
-UIEPaper::UIEPaper() {
+UIEPaper::UIEPaper() : display(GxEPD2_213_B72(14, 27, 33, -1)) {
+	display.init(115200);
   // TODO
-  GxEPD2_BW<GxEPD2_213_B72, GxEPD2_213_B72::HEIGHT> display(GxEPD2_213_B72(14, 27, 33, -1)); // GDEH0213B72
-  display.init(115200);
-
+  //Serial.println("helloWorld");
+  const char text[] = "Hello World!";
+  // most e-papers have width < height (portrait) as native orientation, especially the small ones
+  // in GxEPD2 rotation 0 is used for native orientation (most TFT libraries use 0 fix for portrait orientation)
+  // set rotation to 1 (rotate right 90 degrees) to have enough space on small displays (landscape)
   display.setRotation(1);
+  // select a suitable font in Adafruit_GFX
   display.setFont(&FreeMonoBold9pt7b);
+  // on e-papers black on white is more pleasant to read
   display.setTextColor(GxEPD_BLACK);
-  int16_t tbx, tby; uint16_t tbw, tbh;
-  display.getTextBounds("Hello PIO", 0, 0, &tbx, &tby, &tbw, &tbh);
+  // Adafruit_GFX has a handy method getTextBounds() to determine the boundary box for a text for the actual font
+  int16_t tbx, tby; uint16_t tbw, tbh; // boundary box window
+  display.getTextBounds(text, 0, 0, &tbx, &tby, &tbw, &tbh); // it works for origin 0, 0, fortunately (negative tby!)
   // center bounding box by transposition of origin:
   uint16_t x = ((display.width() - tbw) / 2) - tbx;
   uint16_t y = ((display.height() - tbh) / 2) - tby;
+  // full window mode is the initial mode, set it anyway
   display.setFullWindow();
+
+  display.fillScreen(GxEPD_WHITE); // set the background to white (fill the buffer with value for white)
+  display.setCursor(x, y); // set the postition to start printing text
+  display.print(text); // print some text
+  display.display(false);
+
+  // here we use paged drawing, even if the processor has enough RAM for full buffer
+  // so this can be used with any supported processor board.
+  // the cost in code overhead and execution time penalty is marginal
+  // tell the graphics class to use paged drawing mode
+  /*
   display.firstPage();
   do
   {
-    display.fillScreen(GxEPD_WHITE);
-    display.setCursor(x, y);
-    display.print("Hello PIO");
+    // this part of code is executed multiple times, as many as needed,
+    // in case of full buffer it is executed once
+    // IMPORTANT: each iteration needs to draw the same, to avoid strange effects
+    // use a copy of values that might change, don't read e.g. from analog or pins in the loop!
+    display.fillScreen(GxEPD_WHITE); // set the background to white (fill the buffer with value for white)
+    display.setCursor(x, y); // set the postition to start printing text
+    display.print(text); // print some text
+    // end of part executed multiple times
   }
+  // tell the graphics class to transfer the buffer content (page) to the controller buffer
+  // the graphics class will command the controller to refresh to the screen when the last page has been transferred
+  // returns true if more pages need be drawn and transferred
+  // returns false if the last page has been transferred and the screen refreshed for panels without fast partial update
+  // returns false for panels with fast partial update when the controller buffer has been written once more, to make the differential buffers equal
+  // (for full buffered with fast partial update the (full) buffer is just transferred again, and false returned)
   while (display.nextPage());
-
+  */
+  //Serial.println("helloWorld done");
 }
 
 Command UIEPaper::nextCommand() {
