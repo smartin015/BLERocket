532a6e83b2ca53d31b713d727405867eda715b6f
Jeff Cooper <jeffcooper@google.com>
2019-09-08 23:41:34 -0400

    mission confirmatio page

--- lib/ui/pages/mission_confirm.cpp
+++ lib/ui/pages/mission_confirm.cpp
@@ -3,7 +3,67 @@
 
 // Player about to confirm mission
 void UI::drawMissionConfirm(const Engine* engine) {
-  drawText("Confirm Mission", TITLE_SZ, TITLE_X, TITLE_Y);
-  // TODO Details
-  drawText("Launch Y/N?", TITLE_SZ, BODY_X, BODY_Y);
+  const game::StateT* state = engine->getState();
+
+  int x_offset = SIDEBAR_WIDTH;
+  int y_offset = 0;
+
+  DrawSidebarText("", true);
+
+  setFont(&FONT_TINY);
+
+  DrawStringAt(
+      "< type",
+      0, y_offset,
+      NULL, &y_offset);
+  x_offset += SIDEBAR_MARGIN;
+  setFont(&FONT_POPPINS_8);
+  DrawStringAt(
+      "Confirm Mission",
+      x_offset, y_offset,
+      NULL, &y_offset);
+  y_offset += 3 * LINESPACING;
+  x_offset += 5;
+
+  setFont(&FONT_ROBOTO_6);
+
+  char buf[32];
+  snprintf(buf, sizeof(buf),
+      "Send %s on a mission", 
+      state->ships[state->selectedShip]->name.c_str());
+
+  DrawStringAt(
+      buf,
+      x_offset, y_offset,
+      NULL, &y_offset);
+  snprintf(buf, sizeof(buf),
+      "to %s", 
+      message::EnumNameType(engine->getMission()->type));
+
+  DrawStringAt(
+      buf,
+      x_offset, y_offset,
+      NULL, &y_offset);
+
+  snprintf(buf, sizeof(buf),
+      "in %s@'s system?", 
+      engine->getData()->users[state->selectedUser]->username.c_str());
+  DrawStringAt(
+      buf,
+      x_offset, y_offset,
+      NULL, &y_offset);
+
+  y_offset +=  2*LINESPACING;
+  x_offset += 30;
+
+  setFont(&FONT_ROBOTO_8);
+  DrawStringAt(
+      "X  Confirm",
+      x_offset, y_offset,
+      NULL, &y_offset);
+  y_offset +=  LINESPACING;
+  DrawStringAt(
+      "<  Go back",
+      x_offset, y_offset,
+      NULL, &y_offset);
 }
